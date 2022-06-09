4e106bbc864c1fd615bae56cd97d7493e01736ed
lanphuong987 <1851050118phuong@ou.edu.vn>
2022-06-09 12:24:12 +0700

    f

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi_test_db'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'Phuong123%'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+    debug: false,
+  },
+});
